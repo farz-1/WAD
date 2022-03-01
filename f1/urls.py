4ec0ce7a5f9b93afca04f9d87060d720b8a30ca3
“Farzwan <2553017m@student.gla.ac.uk>
2022-03-01 16:20:42 +0000

    commit

--- f1/urls.py
+++ f1/urls.py
@@ -1,4 +1,4 @@
-"""f1 URL Configuration
+][;"""f1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
