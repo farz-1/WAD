d26e877abb621aa405b1d63d2302ad5c0ed1ca74
Vaughn Muirhead <2575235m@student.gla.ac.uk>
2022-03-07 10:05:43 +0000

    Modified models.py, added average calculation.

--- f1/models.py
+++ f1/models.py
@@ -3,18 +3,97 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+
+class Driver(models.Model):
+    name = models.CharField(max_length=50,null=False)
+    DOB = models.DateField(null=False)
+    picture = models.ImageField(null=False)
+    height = models.CharField(max_length=5,null=False)
+    weight = models.CharField(max_length=5,null=False)
+    nationality = models.CharField(max_length=20,null=False)
+    driverNumber = models.CharField(max_length=2,null=False)
+    seasonsWon = models.IntegerField(null=False)
+    
+    class Meta:
+        verbose_name_plural = 'Drivers'
+        
+    def __str__(self):
+        return self.name
+
+
+class Constructor(models.Model):
+    name = models.CharField(max_length=50,null=False,unique=True)
+    teamPrincipal = models.CharField(max_length=50,null=False)
+    nationality = models.CharField(max_length=20,null=False)
+    yearsActive = models.IntegerField(null=False)
+    raceEngineer = models.CharField(max_length=50,null=False)
+    
+    class Meta:
+        verbose_name_plural = 'Constructors'
+        
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=30,null=False,unique=True)
+    horsepower = models.CharField(max_length=5,null=False)
+    engineSupplier = models.CharField(max_length=30,null=False)
+    picture = models.ImageField(null=False)
+    gearbox = models.CharField(max_length=30,null=False)
+    
+    class Meta:
+        verbose_name_plural = 'Cars'
+        
+    def __str__(self):
+        return self.model
+
+
+class Race(models.Model):
+    location = models.CharField(max_length=20,null=False,unique=True)
+    trackLength = models.CharField(max_length=5,null=False)
+    date = models.DateField(null=False)
+    laps = models.CharField(max_length=5,null=False)
+    raceWeekend = models.CharField(max_length=20,null=False)
+    time = models.CharField(max_length=20,null=False)
+    duration = models.FloatField(null=False)
+    
+    class Meta:
+        verbose_name_plural = 'Races'
+        
+    def __str__(self):
+        return self.location
+
+
+class User(models.Model):
+    username = models.CharField(max_length=30,null=False,unique=True)
+    password = models.CharField(max_length=30,null=False)
+    userID = models.CharField(max_length=30,null=False,unique=True)
+    picture = models.ImageField()
+    favCar = models.ForeignKey(Car, on_delete=models.SET_NULL, null=True)
+    favTeam = models.ForeignKey(Constructor, on_delete=models.SET_NULL, null=True)
+    favDriver = models.ForeignKey(Driver, on_delete=models.SET_NULL, null=True)
+    aboutMe = models.CharField(max_length=256)
+    
+    class Meta:
+        verbose_name_plural = 'Users'
+        
+    def __str__(self):
+        return self.userID + " " + self.name
+
+
 class DriverRating(models.Model):
     driverID = models.ForeignKey(Driver, on_delete=models.CASCADE, null=False)
-    
+
     userID = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-    
+
     created = models.DateTimeField(editable=False,null=False)
-    
+
     lastModified = models.DateTimeField(null=False)
-    
-    #overall average + all 
-    overallAverage = 
-    
+
+    #overall average + all
+    overallAverage = models.FloatField()
+
     overallRating = models.IntegerField(
         default=1,
         validators=[
@@ -22,7 +101,7 @@ class DriverRating(models.Model):
             MinValueValidator(1)
         ],null=False
      )
-    
+
     personality = models.IntegerField(
         default=1,
         validators=[
@@ -72,21 +151,28 @@ class DriverRating(models.Model):
             MinValueValidator(1)
         ],null=False
      )
-    
+
     class Meta:
         verbose_name_plural = 'DriverRatings'
-        
+
+    @property
+    def get_overall_average(self):
+        scores = [self.overallRating, self.personality, self.aggressiveness, self.awareness, self.experience,
+                  self.starts, self.pace, self.racecraft] # add new fields here.
+        return sum(scores)/len(scores) # this way allows for easier adding of new fields in future.
+
     def __str__(self):
         return self.userID + " " + self.driverID
-    
+
     def save(self, *args, **kwargs):
+        self.overallAverage = self.get_overall_average
         ''' On save, update timestamps '''
         if not self.id:
             self.created = timezone.now()
         self.modified = timezone.now()
         return super(User, self).save(*args, **kwargs)
-    
-    
+
+
 class ConstructorRating(models.Model):
     constructorID = models.ForeignKey(Constructor, on_delete=models.CASCADE,null=False)
 
@@ -96,7 +182,7 @@ class ConstructorRating(models.Model):
 
     lastModified = models.DateTimeField(null=False)
 
-    overallAverage =
+    overallAverage = models.FloatField()
 
     overallRating = models.IntegerField(
         default=1,
@@ -129,19 +215,26 @@ class ConstructorRating(models.Model):
             MinValueValidator(1)
         ],null=False
     )
-    
+
     class Meta:
         verbose_name_plural = 'ConstructorRatings'
-        
+
+    @property
+    def get_overall_average(self):
+        scores = [self.overallRating, self.teamPrinciple, self.raceStrategy, self.pitStop]  # add new fields here.
+        return sum(scores) / len(scores)  # this way allows for easier adding of new fields in future.
+
     def __str__(self):
         return self.userID + " " + self.constructorID
 
     def save(self, *args, **kwargs):
+        self.overallAverage = self.get_overall_average
         ''' On save, update timestamps '''
         if not self.id:
             self.created = timezone.now()
         self.modified = timezone.now()
         return super(User, self).save(*args, **kwargs)
+
 
 class CarRating(models.Model):
     carID = models.ForeignKey(Car, on_delete=models.CASCADE,null=False)
@@ -152,7 +245,7 @@ class CarRating(models.Model):
 
     lastModified = models.DateTimeField(null=False)
 
-    overallAverage =
+    overallAverage = models.FloatField()
 
     overallRating = models.IntegerField(
         default=1,
@@ -178,7 +271,7 @@ class CarRating(models.Model):
         ],null=False
     )
 
-    Aesthetics = models.IntegerField(
+    aesthetics = models.IntegerField(
         default=1,
         validators=[
             MaxValueValidator(5),
@@ -201,89 +294,23 @@ class CarRating(models.Model):
             MinValueValidator(1)
         ],null=False
     )
-    
+
     class Meta:
         verbose_name_plural = 'CarRatings'
-        
+
+    @property
+    def get_overall_average(self):
+        scores = [self.overallRating, self.speed, self.aerodynamics, self.aesthetics, self.braking,
+                  self.engine]  # add new fields here.
+        return sum(scores) / len(scores)  # this way allows for easier adding of new fields in future.
+
     def __str__(self):
         return self.userID + " " + self.carID
 
     def save(self, *args, **kwargs):
+        self.overallAverage = self.get_overall_average
         ''' On save, update timestamps '''
         if not self.id:
             self.created = timezone.now()
         self.modified = timezone.now()
         return super(User, self).save(*args, **kwargs)
-
-class Driver(models.Model):
-    name = models.CharField(max_length=50,null=False)
-    DOB = models.DateField(null=False)
-    picture = models.ImageField(null=False)
-    height = models.CharField(max_length=5,null=False)
-    weight = models.CharField(max_length=5,null=False)
-    nationality = models.CharField(max_length=20,null=False)
-    driverNumber = models.CharField(max_length=2,null=False)
-    seasonsWon = models.IntegerField(max_length=5,null=False)
-    
-    class Meta:
-        verbose_name_plural = 'Drivers'
-        
-    def __str__(self):
-        return self.name
-
-class Constructor(models.Model):
-    name = models.CharField(max_length=50,null=False,unique=True)
-    teamPrincipal = models.CharField(max_length=50,null=False)
-    nationality = models.CharField(max_length=20,null=False)
-    yearsActive = models.IntegerField(max_length=2,null=False)
-    raceEngineer = models.CharField(max_length=50,null=False)
-    
-    class Meta:
-        verbose_name_plural = 'Constructors'
-        
-    def __str__(self):
-        return self.name
-    
-class Car(models.Model):
-    model = models.CharField(max_length=30,null=False,unique=True)
-    horsepower = models.CharField(max_length=5,null=False)
-    engineSupplier = models.CharField(max_length=30,null=False)
-    picture = models.ImageField(null=False)
-    gearbox = models.CharField(max_length=30,null=False)
-    
-    class Meta:
-        verbose_name_plural = 'Cars'
-        
-    def __str__(self):
-        return self.model
-    
-class Race(models.Model):
-    location = models.CharField(max_length=20,null=False,unique=True)
-    trackLength = models.CharField(max_length=5,null=False)
-    date = models.DateField(null=False)
-    laps = models.CharField(max_length=5,null=False)
-    raceWeekend = models.CharField(max_length=20,null=False)
-    time = models.CharField(max_length=20,null=False)
-    duration = models.FloatField(null=False)
-    
-    class Meta:
-        verbose_name_plural = 'Races'
-        
-    def __str__(self):
-        return self.location
-
-class User(models.Model):
-    username = models.CharField(max_length=30,null=False,unique=True)
-    password = models.CharField(max_length=30,null=False)
-    userID = models.CharField(max_length=30,null=False,unique=True)
-    picture = models.ImageField()
-    favCar = models.ForeignKey(Car, on_delete=models.SET_NULL, null=False)
-    favTeam = models.ForeignKey(Constructor, on_delete=models.SET_NULL, null=False)
-    favDriver = models.ForeignKey(Driver, on_delete=models.SET_NULL, null=False)
-    aboutMe = models.CharField(max_length=256)
-    
-    class Meta:
-        verbose_name_plural = 'Users'
-        
-    def __str__(self):
-        return self.userID + " " + self.name
