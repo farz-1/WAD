26dbee3de66280f548a3019968dd2e185e8f6e9d
“Farzwan <2553017m@student.gla.ac.uk>
2022-03-22 19:16:37 +0000

    updated tasks

--- for1/views_profile.py
+++ for1/views_profile.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'for1/users/user.profile.html', {'user': request.user})
+    return render(request, 'for1/profile/profile.html', {'user': request.user})
