238ffbdbadac093db9e8be69e10c722e958ebf7c
“Farzwan <2553017m@student.gla.ac.uk>
2022-03-23 22:12:31 +0000

    added driver and constructor rating forms

--- for1/views_constructors.py
+++ for1/views_constructors.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from pyexpat.errors import messages
+
+from for1.forms import ConstructorRatingForm
 from for1.models import Constructor, Driver
 
 
@@ -32,4 +35,36 @@ def details(request, slug):
 
 
 def rate(request, slug):
-    pass
+    context_dict = {}
+    if request.method == 'POST':
+        print(request.POST)
+        print('RATE WITH POST')
+        rating_form =ConstructorRatingForm(request.POST)
+        context_dict['rating_form'] = rating_form
+
+        print("XXXXXXXXXXXXXXXXXXxxxxxxxxxxxxxxxxxxxxxxxxxXx\n\n")
+        print(rating_form.errors.as_data())
+        print("XXXXXXXXXXXXXXXXXXxxxxxxxxxxxxxxxxxxxxxxxxxxXx\n\n")
+        print(rating_form)
+        if rating_form.is_valid():
+            rating = rating_form.save(commit=False)
+            print(rating)
+            rating.userID = request.user
+            rating.conID=Constructor.objects.get(slug=slug)
+            rating.save()
+            messages.success(request, "Rating submitted successfully")
+            return redirect('constructors')
+        else:
+            messages.error(request, "Rating could not be submitted. Please try again")
+            return redirect('constructor_rate', slug)
+    else:
+        context_dict = {}
+        rating_form=ConstructorRatingForm()
+        context_dict['rating_form'] = rating_form
+        try:
+            constructor = Constructor.objects.get(slug=slug)
+            context_dict['constructor'] = constructor
+
+        except Constructor.DoesNotExist:
+            context_dict['constructor'] = None
+    return render(request, 'for1/constructors/constructor.rating_form.html.html', context=context_dict)
--- for1/views_drivers.py
+++ for1/views_drivers.py
@@ -28,4 +28,42 @@ def details(request, slug):
     return render(request, 'for1/drivers/driver.details.html', context=context_dict)
 
 def rate(request, slug):
-    pass
+    context_dict = {}
+    if request.method == 'POST':
+        print(request.POST)
+        print('RATE WITH POST')
+        rating_form = DriverRatingForm(request.POST)
+        context_dict['rating_form'] = rating_form
+
+        print("XXXXXXXXXXXXXXXXXXxxxxxxxxxxxxxxxxxxxxxxxxxXx\n\n")
+        print(rating_form.errors.as_data())
+        print("XXXXXXXXXXXXXXXXXXxxxxxxxxxxxxxxxxxxxxxxxxxxXx\n\n")
+        print(rating_form)
+
+        if rating_form.is_valid():
+            rating = rating_form.save(commit=False)
+            print(rating)
+            rating.userID = request.user
+            rating.carID = drivers.objects.get(slug=slug)
+            rating.save()
+
+            messages.success(request, "Rating submitted successfully")
+            return redirect('drivers')
+
+        else:
+            messages.error(request, "Rating could not be submitted. Please try again")
+            return redirect('driver_rate', slug)
+
+    else:
+        context_dict = {}
+        rating_form = DriverRatingForm()
+        context_dict['rating_form'] = rating_form
+
+        try:
+            drivers = drivers.objects.get(slug=slug)
+            context_dict['drivers'] = drivers
+
+        except Driver.DoesNotExist:
+            context_dict['drivers'] = None
+
+        return render(request, 'for1/drivers/driver.rating_form.html', context=context_dict)
