5ef236b2f3b0e71a7cb2234684138ec5a8187328
“Farzwan <2553017m@student.gla.ac.uk>
2022-03-23 22:51:25 +0000

    fix2

--- for1/views_drivers.py
+++ for1/views_drivers.py
@@ -16,7 +16,7 @@ def index(request):
     return render(request, 'for1/drivers/drivers.html', context=context_dict)
 
 
-def driver_details(request, slug):
+def details(request, slug):
     context_dict = {}
 
     try:
@@ -27,3 +27,7 @@ def driver_details(request, slug):
         context_dict['driver'] = None
 
     return render(request, 'for1/drivers/driver.details.html', context=context_dict)
+
+
+def rate(request, slug):
+    pass
